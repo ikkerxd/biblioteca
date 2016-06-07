a252f5101f01213ec9b45f68defa791775632d13
Marycel <marycel.u.q@gmail.com>
2016-06-06 22:20:59 -0500

    admin usuarios

--- biblioteca/apps/users/admin.py
+++ biblioteca/apps/users/admin.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
 from .models import User
 
-class UserAdmin(admin.ModelAdmin):
-    list_display = ['username', 'email', 'first_name', 'last_name', 'is_active', 'is_staff']
-    ordering = ['username']
-    search_fields = ('username',)
-# Register your models here.
+class UserAdmin(UserAdmin):
+	fieldsets = (
+		('Usuario', {'fields' : ('username', 'password')}),
+		('Informacion del usuario' , {'fields' : ('first_name',
+										'last_name',
+										'email',
+										'avatar')}),
+		('Permisos' , {'fields' : ('is_active',
+										'is_staff',
+										'is_superuser',
+										'groups',
+										'user_permissions')}),
+
+		)
 admin.site.register(User, UserAdmin)
+
--- biblioteca/apps/users/forms.py
+++ biblioteca/apps/users/forms.py
@@ -2,6 +2,7 @@
 from django import forms
 
 from django.contrib.auth import authenticate
+from .models import User
 
 
 class LoginForm(forms.Form):
